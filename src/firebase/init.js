0c89df73dc33242a18373cca5f21c7876caf2516
Tyler Bray <broncobray77@gmail.com>
2018-05-22 15:49:06 -0700

    maps api renders

---
+++ src/firebase/init.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCvlMB0BngQOxvqbCIBeBAbCemc-d5Y3Ys",
+    authDomain: "geo-ninjas-19466.firebaseapp.com",
+    databaseURL: "https://geo-ninjas-19466.firebaseio.com",
+    projectId: "geo-ninjas-19466",
+    storageBucket: "",
+    messagingSenderId: "696251446970"
+};
+const firebaseApp = firebase.initializeApp(config); 
+
+export default firebaseApp.firestore()
